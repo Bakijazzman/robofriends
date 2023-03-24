ed75b49c6828ea6aa518b2edd6c2d23ff7b0820a
Bakijazzman <obafemiolabayo@gmail.com>
2023-03-24 21:16:25 +0100

    hmmmm

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import 'tachyons'
 
 const App =()=>{
     return (
-        <div>
+        <div className="tc ">
             <h1>ROBOFRIENDS</h1>
-            <input type={"search"} placeholder='search robots'/>
+            <input type={"search"} placeholder='search robots' className="pa3 ba b--green bg-lightest-blue"/>
             <CardList robots = {robots}/>
         </div>
     )
