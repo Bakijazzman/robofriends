f677517a06e84d82a4df682adaf1cfe44c43d3e8
Bakijazzman <obafemiolabayo@gmail.com>
2023-03-24 20:24:28 +0100

    check

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App'
 import 'tachyons'
-import { robots } from './robots'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
